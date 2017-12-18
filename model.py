916d8e31ba7bde71d42955656782b35f877c4600
Dani Gámez Franco <gmzcodes@gmail.com>
2017-12-18 21:34:47 +0100

    Increases epochs from 10 to 16 and fixes some minor issues on the code.

--- model.py
+++ model.py
@@ -57,7 +57,7 @@ def generator(samples, batch_size=CONST.GENERATOR_BATCH_SIZE):
                 name_right = batch_sample["right"]
 
                 # Center Image:
-		
+
                 image_center_bgr = cv2.imread(name_center)
                 image_center = cv2.cvtColor(image_center_bgr, cv2.COLOR_BGR2YUV)
                 # image_center = image_center[...,::-1]
@@ -170,4 +170,4 @@ plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-plt.savefig('history.png')
+plt.show()
